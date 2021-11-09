6f7c255a78bacbd740d64ec3af6980f4699feee6
Luis Torres <Luis@Torres.com>
2021-11-08 19:09:28 -0500

    authentication created

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { User } from './users/entities/user.entity';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 
 const entities = [User];
@@ -24,7 +25,8 @@ const entities = [User];
       entities: entities,
       synchronize: true,
     }),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
